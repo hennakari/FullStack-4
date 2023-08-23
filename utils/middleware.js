42eb3c5f75b5c7325f0441cb848569a656cc6b5a
hkari <hkari@jyu.fi>
2023-08-23 15:51:42 +0300

    Teht 4.3

--- utils/middleware.js
+++ utils/middleware.js
@@ -10,7 +10,7 @@ const requestLogger = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-  
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
@@ -18,7 +18,7 @@ const errorHandler = (error, request, response, next) => {
   }
   next(error)
 }
-  
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
