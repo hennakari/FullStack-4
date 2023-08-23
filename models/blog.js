baf56ed8989a219e645510f11cd6c73e8d450446
hkari <hkari@jyu.fi>
2023-08-23 14:53:58 +0300

    Teht 4.2

--- models/blog.js
+++ models/blog.js
@@ -1,20 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
+//const Blog = mongoose.model('Blog', blogSchema)
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const blogSchema = mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
@@ -28,34 +16,5 @@ blogSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-/* const peopleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minlength: 8,
-    required: true,
-    validate: {
-      validator: function(v) {
-        return /^[0-9]{2,3}-[0-9]{1,}$/.test(v)
-      },
-      message: props => `${props.value} is not a valid phone number!`
-    }
-  }
-}) */
-
-/* peopleSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-}) */
 
 module.exports = mongoose.model('Blog', blogSchema)
